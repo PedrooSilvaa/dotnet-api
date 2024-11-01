b73812651e5e6dd3351f1b2d4534069ec0db972c
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-10-31 21:35:38 -0300

    feat: add validation in CategoryPut

--- src/obj/Debug/net8.0/IWantApp.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/IWantApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IWantApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4805d91d3ef88d2779ab0939677142c5c8ff770a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e3022b8e9d6a8395ff5a978a2d1432d3dd6a016")]
 [assembly: System.Reflection.AssemblyProductAttribute("IWantApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IWantApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
